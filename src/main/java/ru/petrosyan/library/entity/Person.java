867c1cb499826ce5ce35ee7830adc2629ebf9868
armen <armenpetrosan319@gmail.com>
2026-01-21 13:42:49 +0300

    feat(person entity): added validation for Person.java

--- src/main/java/ru/petrosyan/library/entity/Person.java
+++ src/main/java/ru/petrosyan/library/entity/Person.java
@@ -1,12 +1,24 @@
 package ru.petrosyan.library.entity;
 
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.List;
 
 public class Person {
     private Integer id;
+
+    @NotBlank(message = "ФИО обязательное поле!")
+    @Size(message = "ФИО не может содержать больше 70 символов или меньше 10", max=70, min = 10)
     private String fio;
+
+    @NotNull(message = "Дата обязательна для выбора!")
+    @Past(message = "Дата не может быть в будущем!")
     private LocalDate dateBirth;
+
     private List<Book> books;
 
     public Person(Integer id, String fio, LocalDate dateBirth) {
