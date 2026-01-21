e9b17bac8ce4f0379fa376cf2fc079cda4a1514f
armen <armenpetrosan319@gmail.com>
2026-01-21 20:54:49 +0300

    fetch: работает изменение книги, bookDao содержит метод получения по id

--- src/main/java/ru/petrosyan/library/dao/BookDAO.java
+++ src/main/java/ru/petrosyan/library/dao/BookDAO.java
@@ -12,10 +12,12 @@ import java.util.List;
 @Component
 public class BookDAO {
     private final DataSource dataSource;
+    private final PersonDAO personDAO;
 
     @Autowired
-    public BookDAO(DataSource dataSource) {
+    public BookDAO(DataSource dataSource, PersonDAO personDAO) {
         this.dataSource = dataSource;
+        this.personDAO = personDAO;
     }
 
     public List<Book> getAllBooks() {
@@ -52,6 +54,49 @@ public class BookDAO {
             }
         } catch (SQLException e) {
             System.out.println("Error database by insert Book: " + e);
+            e.printStackTrace();
+        }
+    }
+
+    public Book getBookById(Integer bookId) {
+        Book book = null;
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("select * from book where book_id = ?;")
+        ) {
+            preparedStatement.setInt(1, bookId);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    book = new Book();
+                    book.setId(resultSet.getInt("book_id"));
+                    book.setAuthor(resultSet.getString("author"));
+                    book.setTitle(resultSet.getString("title"));
+                    book.setDateCreate(resultSet.getDate("datecreate").toLocalDate());
+                    book.setPerson(personDAO.getPersonById(resultSet.getInt("person_id")));
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error database by get Book: " + e);
+            e.printStackTrace();
+        }
+        return book;
+    }
+
+    public void updateBook(Book book) {
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("update book set title=?, author=?, datecreate=? where book_id=?;")
+        ) {
+            preparedStatement.setString(1, book.getTitle());
+            preparedStatement.setString(2, book.getAuthor());
+            preparedStatement.setDate(3, Date.valueOf(book.getDateCreate()));
+            preparedStatement.setInt(4, book.getId());
+
+            int row = preparedStatement.executeUpdate();
+            if (row > 0) {
+                System.out.println("Book is update");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error database by update Book: " + e);
+            e.printStackTrace();
         }
     }
 }
