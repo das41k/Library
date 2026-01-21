aa4c29c1e44b2b15065cc7e6cf7afce0c39fc153
armen <armenpetrosan319@gmail.com>
2026-01-21 19:54:35 +0300

    fetch(BookController): функционал добавления книги полностью работает

--- src/main/java/ru/petrosyan/library/conroller/BookController.java
+++ src/main/java/ru/petrosyan/library/conroller/BookController.java
@@ -3,10 +3,15 @@ package ru.petrosyan.library.conroller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.petrosyan.library.dao.BookDAO;
 import ru.petrosyan.library.entity.Book;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/book")
@@ -29,5 +34,14 @@ public class BookController {
     public String formInsertBook(Model model) {
         model.addAttribute("book", new Book());
         return "/book/bookAdd";
+    }
+
+    @PostMapping
+    public String insertBook(@ModelAttribute("book") @Valid Book book, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "book/bookAdd";
+        }
+        bookDAO.insertBook(book);
+        return "redirect:/book";
     }
 }
